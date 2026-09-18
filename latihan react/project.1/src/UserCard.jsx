6d16e8be2e5cbeab07cfe9a2245d1fe4f4cf8b7f
Joan <joantramadhan06@gmail.com>
2026-09-18 09:13:48 +0700

    belajar react 2

---
+++ latihan react/project.1/src/UserCard.jsx
@@ -0,0 +1,9 @@
+function UserCard (props) {
+    return (
+        <div classname = "card">
+            <h3>{props.name}</h3>
+            <p>{props.jabatan}</p>
+        </div>
+    )
+}
+export default UserCard;
